470d40c1e1717d9aed1eff936051cf9f979d7bb7
dpgallivan <danielgallivan4@gmail.com>
2017-08-09 19:25:05 -0700

    Update app.js

--- java/app.js
+++ java/app.js
@@ -1,113 +1,174 @@
-$(document).ready(function() {
+var panel = $("#quiz-area");
 
-var correctAnswers = 0
-var incorrectAnswers = 0
-var unanswered = 0
-var counterNum = 100
-var questionArr = ["How big is the world's population?", //1
-					"What is the number for 911?",//2
- 					"Which country has an orange turd as their President?", //3
- 					"Where was the hottest temperature recorded on Earth?", //4
-  					"What is a zebra's natural skin color?", //5
-  					"A monkey, squirrel, and bird are racing to the top of a coconut tree to get the banana, who will get there first?", //6 
-  					"If you throw a red stone into a blue sea what will it become?"] //7
-var choicesArr = ["7.1,7.2,7.3,7.4", //1
-					"911.,911?,911,unsure", //2
-					"USA, France, UK, Neither", //3
-					"The Flaming Mountains, Death Valley, El Azizia, Australia's Badlands", //4
-					"White, Black, Gray, Twilight", //5
-					"The squirrel, the monkey, the bird, neither", //6
-					"A red stone, a blue stone, a wet stone, a dry stone"] //7
-var answersArr = ["7.4",//1
-				"911",//2
-				"Neither",//3
-				"El Azizia",//4
-				"White",//5
-				"neither",//6
-				"a wet stone"]//7
-var intervalId;
-var triviaGame;
-var number;
-var selectedAnswer;
-var printThis = "";
-// <========================================================================	
-	$("answersArr").on("click", function(event) {
-		selectedAnswer = $(this).text();
-	});
+// Question set
+var questions = [{
+  question: "What was the first full length CGI movie?",
+  answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story"
+}, {
+  question: "Which of these is NOT a name of one of the Spice Girls?",
+  answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
+  correctAnswer: "Fred Spice"
+}, {
+  question: "Which NBA team won the most titles in the 90s?",
+  answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
+  correctAnswer: "Chicago Bulls"
+}, {
+  question: "Which group released the hit song, \"Smells Like Teen Spirit\"?",
+  answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
+  correctAnswer: "Nirvana"
+}, {
+  question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
+  answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
+  correctAnswer: "The Lion King"
+}, {
+  question: "Finish this line from the Fresh Prince of Bel-Air theme song: \"I whistled for a cab and when it came near, the license plate said...\"",
+  answers: ["Dice", "Mirror", "Fresh", "Cab"],
+  correctAnswer: "Fresh"
+}, {
+  question: "What was Doug's best friend's name?",
+  answers: ["Skeeter", "Mark", "Zach", "Cody"],
+  correctAnswer: "Skeeter"
+}, {
+  question: "What was the name of the principal at Bayside High in Saved By The Bell?",
+  answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
+  correctAnswer: "Mr.Belding"
+}];
 
-	for(var i = 0; i < questionArr.length; i++) {
-		printThis += "<br>" + questionArr
-	}
-	return printThis;
+// Variable that will hold the setInterval
+var timer;
 
-	function timer() {
-		intervalId = setInterval(decrement, 4000);
-		function decrement() {
-			number--
-		$("#show-number").html("<h3>" + number + intervalId + "</h3");
-			stop();
-			reset(decrement, 1000);
-		}
-	}
+var game = {
 
-	function LossFromTimeout() {
-		if (number === 0);
-			alert("Time is up!");
-			unanswered++
-		triviaGame = ".panelbody"
-		$(".panelbody").html(triviaGame);
-	}
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
 
-	function amountofWins() {
-		if(answersArr[i] === choicesArr[i])
-			correctAnswers++
-		if (number === 0);
-			alert("Time is up!");
-		triviaGame = ".panelbody"
-		$(".panelbody").html(triviaGame);
-	}
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
 
-	function amountofIncorrect() {
-		if(answersArr[i] != choicesArr[i])
-			incorrectAnswers++
-		if (number === 0);
-			alert("Time is up!");
-		triviaGame = ".panelbody"
-		$(".panelbody").html(triviaGame);
-	}
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
 
-	function generateHTML() {
-		$("questionArr" + "choicesArr").html();
-	}
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    panel.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).on("click", "#done", function() {
+  game.done();
 });
